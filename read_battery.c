2ea3108210544b41036be72b883b1adf5505f41e
Frank Adams <thedalles77@gmail.com>
2018-11-30 23:12:03 -0800

    Update read_battery.c

--- read_battery.c
+++ read_battery.c
@@ -1,4 +1,16 @@
-// This software is in the public domain.
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 // The program reads the laptop battery status registers over a bit-
 // bang SMBus created with two of the Pi's GPIO pins and wiringPi. 
 // SMBus Data and clock are pulled to 3.3 volts with 3K resistors.
@@ -19,6 +31,7 @@
 // Rev 1.0 - Dec 25, 2017 - Original Release
 // Rev 1.1 - Jan 1, 2018 - Add rev history and public domain
 // Rev 1.2 - March 7, 2018 - Added test for out of range on bat current
+// Rev 1.3 - Nov 30, 2018 - Added Apache License Header
 //
 #include <stdio.h>
 #include <wiringPi.h>
